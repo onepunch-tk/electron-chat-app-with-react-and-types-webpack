c90c3f247d2e07628156d5d0b8c5168cee8ccc26
TaekyungHa <86tkstar@gmail.com>
2022-08-25 21:50:04 +0900

    v1.1.1

---
+++ src/renderer/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+    ReactDOM.render(<h2>Im React App</h2>, document.getElementById('electron-root'));
+}
+
+render();
